fa6cd862aec1ef6b75228a0d42efc572ae3bb100
segzyswitch <segzyswitch99@gmail.com>
2026-02-21 13:16:13 +0100

    smartsupp chat!

--- app/plugins/smartsupp.client.ts
+++ app/plugins/smartsupp.client.ts
@@ -7,8 +7,7 @@ export default defineNuxtPlugin(() => {
       var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
       s=d.getElementsByTagName('script')[0];c=d.createElement('script');
       c.type='text/javascript';c.charset='utf-8';c.async=true;
-      c.src='https://www.smartsuppchat.com/loader.js?';
-      s.parentNode.insertBefore(c,s);
+      c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
     })(document);
   `
   document.head.appendChild(script)
